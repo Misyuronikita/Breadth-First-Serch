09070486c0e2462a6ceabe1a3f3d002156b0c2aa
unknown <fixik2019@gmail.com>
2023-07-01 00:19:37 +0300

    save

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,21 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Vertex<Integer> v0 = new Vertex<>(0);
-        Vertex<Integer> v1 = new Vertex<>(1);
-        Vertex<Integer> v2 = new Vertex<>(2);
-        Vertex<Integer> v3 = new Vertex<>(3);
-        Vertex<Integer> v4 = new Vertex<>(4);
-        Vertex<Integer> v5 = new Vertex<>(5);
-        Vertex<Integer> v6 = new Vertex<>(6);
+        Vertex<String> v0 = new Vertex<>("Я");
+        Vertex<String> v1 = new Vertex<>("Эрик");
+        Vertex<String> v2 = new Vertex<>("Леха");
+        Vertex<String> v3 = new Vertex<>("Илья");
+        Vertex<String> v4 = new Vertex<>("Макс");
+        Vertex<String> v5 = new Vertex<>("Кирилл");
+        Vertex<String> v6 = new Vertex<>("Андрей");
+        Vertex<String> v7 = new Vertex<>("Дима");
 
-        Breadth_First_Search<Integer> breadthFirstSearch = new Breadth_First_Search<>(v0);
-        v0.setNeighbors(new LinkedList<>(Arrays.asList(v1, v5, v6)));
-        v1.setNeighbors(new LinkedList<>(Arrays.asList(v3, v4, v5)));
-        v4.setNeighbors(new LinkedList<>(Arrays.asList(v2, v6)));
-        v6.setNeighbors(new LinkedList<>(List.of(v1)));
+
+        Breadth_First_Search<String> breadthFirstSearch = new Breadth_First_Search<>(v0);
+        v0.setNeighbors(new LinkedList<>(Arrays.asList(v1, v2, v3)));
+        v1.setNeighbors(new LinkedList<>(Arrays.asList(v4, v7)));
+        v2.setNeighbors(new LinkedList<>(List.of(v4)));
+        v3.setNeighbors(new LinkedList<>(Arrays.asList(v5, v6)));
         breadthFirstSearch.traverse();
     }
 }
