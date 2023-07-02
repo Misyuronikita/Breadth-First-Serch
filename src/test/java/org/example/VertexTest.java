54d70682bd30bdbd991e22d72a00c20e8e0b41b0
unknown <fixik2019@gmail.com>
2023-07-02 11:31:01 +0300

    improve unit-tests

--- src/test/java/org/example/VertexTest.java
+++ src/test/java/org/example/VertexTest.java
@@ -1,62 +1,63 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VertexTest {
+    Vertex<String> vertex;
+    Vertex<String> expectedVertexNeighborName1;
+    String firstNameNeighbor;
+    Vertex<String> expectedVertexNeighborName2;
+    String secondNameNeighbor;
+    String expectedData;
+    String anotherExpectedData;
+    boolean expectedVertexIsVisited;
+    @org.junit.jupiter.api.BeforeEach
+    void prepareData(){
+        expectedData = "Виталий";
+        anotherExpectedData = "Константин";
+
+        firstNameNeighbor = "Яна";
+        secondNameNeighbor = "Евгений";
+
+
+        vertex = new Vertex<>(expectedData);
+
+        expectedVertexNeighborName1 = new Vertex<>(firstNameNeighbor);
+        expectedVertexNeighborName2 = new Vertex<>(secondNameNeighbor);
+    }
 
     @org.junit.jupiter.api.Test
     void testGetVertexDataMethod() {
-        String expectedValue = "Виталик";
-        Vertex<String> vertex = new Vertex<>(expectedValue);
-        Assertions.assertEquals("Виталик", vertex.getData());
+        Assertions.assertEquals(expectedData, vertex.getData());
     }
 
     @org.junit.jupiter.api.Test
     void testSetVertexDataMethod() {
-        String defaultValue = "Юлия";
-        String expectedValue = "Виталий";
-        Vertex<String> vertex = new Vertex<>(defaultValue);
-        vertex.setData(expectedValue);
-        assertEquals(expectedValue, vertex.getData());
+        vertex.setData(anotherExpectedData);
+        assertEquals(anotherExpectedData, vertex.getData());
     }
 
     @org.junit.jupiter.api.Test
-    void isVisited() {
-        boolean expectedValue = true;
-        Vertex<String> vertex = new Vertex<>("Артем");
-        vertex.setVisited(expectedValue);
-        Assertions.assertEquals(expectedValue, vertex.isVisited());
-    }
-
-    @org.junit.jupiter.api.Test
-    void setVisited() {
-        boolean expectedValue = true;
-        Vertex<Integer> vertex = new Vertex<>(15);
-        vertex.setVisited(expectedValue);
-        assertEquals(expectedValue, vertex.isVisited());
+    void testSetVisitedMethod() {
+        vertex = new Vertex<>(expectedData);
+        vertex.setVisited(expectedVertexIsVisited);
+        assertEquals(expectedVertexIsVisited, vertex.isVisited());
     }
 
     @org.junit.jupiter.api.Test
     void testSetNeighborsMethod() {
-        Vertex<String> vertex1 = new Vertex<>("Александр");
-        Vertex<String> vertex2 = new Vertex<>("Яна");
-        Vertex<String> vertex3 = new Vertex<>("Евгений");
-
         List<Vertex<String>> vertexNeighbordList = new LinkedList<>();
-        vertexNeighbordList.add(vertex2);
-        vertexNeighbordList.add(vertex3);
+        vertexNeighbordList.add(expectedVertexNeighborName1);
+        vertexNeighbordList.add(expectedVertexNeighborName2);
 
-        vertex1.setNeighbors(vertexNeighbordList);
+        vertex.setNeighbors(vertexNeighbordList);
 
         assertEquals(2, vertexNeighbordList.size());
-        assertEquals(vertexNeighbordList, vertex1.getNeighbors());
+        assertEquals(vertexNeighbordList, vertex.getNeighbors());
     }
 }
